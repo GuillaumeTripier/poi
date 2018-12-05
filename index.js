5b4204d0dacc9c0c55584faa956f39de59ff8811
GuillaumeTripier <guillaume.tripier1@gmail.com>
2018-12-05 15:50:04 +0100

    executed : .\node_modules\.bin\eslint --init

--- index.js
+++ index.js
@@ -5,4 +5,6 @@ const app = express();
 
 app.get('/', (req, res) => res.json(data));
 
+app.get('/:search', (req, res) => res.json(data.filter((poi) => poi.title.toLowerCase().indexOf(req.params.search.toLowerCase()) !== -1 || (poi.details !== undefined ? poi.details.toLowerCase().indexOf(req.params.search.toLowerCase()) !== -1 : false))) );
+
 app.listen(process.env.PORT, () => console.log('Listening...'));
